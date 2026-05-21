42abddd213ae9d2026fb4010edc4c019f1062762
tomines-jaeyoung <tominesss12@gmail.com>
2026-05-21 14:52:37 +0900

    옵션메뉴클릭, 색변경

--- app/src/main/java/kr/ac/kopo/menutest1/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/menutest1/MainActivity.java
@@ -1,14 +1,26 @@
 package kr.ac.kopo.menutest1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button btn;
+
+    int rotationDegree;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        linear = findViewById(R.id.main);
+        btn = findViewById(R.id.btn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.item_yellow) {
+            linear.setBackgroundColor(Color.YELLOW);
+            return true;
+        } else if (item.getItemId() == R.id.item_pink) {
+            linear.setBackgroundColor(Color.MAGENTA);
+            return true;
+        } else if (item.getItemId() == R.id.item_blue) {
+            linear.setBackgroundColor(Color.BLUE);
+            return true;
+        } else if (item.getItemId() == R.id.btn_rotate) {
+            rotationDegree += 45;
+            btn.setRotation(rotationDegree);
+            return true;
+        } else if (item.getItemId() == R.id.btn_zoomin) {
+            btn.setScaleX(2);
+            return true;
+        }
+        return false;
     }
 }
