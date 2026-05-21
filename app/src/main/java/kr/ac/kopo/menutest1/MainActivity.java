62acb5b7a09cc97c8807a071cbb9438da87123f1
tomines-jaeyoung <tominesss12@gmail.com>
2026-05-21 16:08:47 +0900

    그림회전

--- app/src/main/java/kr/ac/kopo/menutest1/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/menutest1/MainActivity.java
@@ -1,69 +1,89 @@
 package kr.ac.kopo.menutest1;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout linear;
-    Button btn;
 
-    int rotationDegree;
-
+    EditText editAngle;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        linear = findViewById(R.id.main);
-        btn = findViewById(R.id.btn);
+        // EdgeToEdge 관련 코드 절대 넣지 말 것
+
+        setContentView(R.layout.activity_main);
+
+        editAngle = findViewById(R.id.editAngle);
+        imageView = findViewById(R.id.imageView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.option_menu, menu);
+
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+
         return true;
     }
 
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if (item.getItemId() == R.id.item_yellow) {
-            linear.setBackgroundColor(Color.YELLOW);
-            return true;
-        } else if (item.getItemId() == R.id.item_pink) {
-            linear.setBackgroundColor(Color.MAGENTA);
-            return true;
-        } else if (item.getItemId() == R.id.item_blue) {
-            linear.setBackgroundColor(Color.BLUE);
-            return true;
-        } else if (item.getItemId() == R.id.btn_rotate) {
-            rotationDegree += 45;
-            btn.setRotation(rotationDegree);
-            return true;
-        } else if (item.getItemId() == R.id.btn_zoomin) {
-            btn.setScaleX(2);
+
+        int id = item.getItemId();
+
+        if (id == R.id.item_rotate) {
+
+            String str = editAngle.getText().toString();
+
+            if (!str.isEmpty()) {
+                float angle = Float.parseFloat(str);
+                imageView.setRotation(angle);
+            }
+
             return true;
         }
-        return false;
+
+        else if (id == R.id.item_hanra) {
+
+            item.setChecked(true);
+
+            imageView.setImageResource(R.drawable.jeju1);
+            imageView.setRotation(0);
+
+            return true;
+        }
+
+        else if (id == R.id.item_chuja) {
+
+            item.setChecked(true);
+
+            imageView.setImageResource(R.drawable.jeju2);
+            imageView.setRotation(0);
+
+            return true;
+        }
+
+        else if (id == R.id.item_beom) {
+
+            item.setChecked(true);
+
+            imageView.setImageResource(R.drawable.jeju3);
+            imageView.setRotation(0);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
